f613b7ac605b5f6bc5c3d081326abc108da50cf2
PowerLichen <dltm135@gmail.com>
2021-09-03 21:56:54 +0900

    fix: Change loaduser code

--- back/lib/passport/index.js
+++ back/lib/passport/index.js
@@ -1,14 +1,16 @@
 const passport = require('passport');
 const local = require('./local.js');
 module.exports = (pool) => {
-    passport.serializeUser((user, done) =>{
-        done(null,user.userId);
+    passport.serializeUser((user, done) => {
+        console.log('serialize ' + user.userId);
+        done(null, user.userId);
     });
-    passport.deserializeUser((userId, done)=>{
+    passport.deserializeUser((userId, done) => {
+        console.log('deserialize ' + userId);
         let sql = 'SELECT uid as id, user_id as userId, user_pwd as userPwd, user_nm as nickname FROM userdata_tb WHERE user_id=?';
         pool.query(sql, [userId],
-            (err, rows, fields) =>{
-                if(err){
+            (err, rows, fields) => {
+                if (err) {
                     console.log(err);
                     return done(err);
                 }
